c509b1f62fb2fd2cfde36ac78ba79e319ff38f80
usershokhrukh <asxurovsit@gmail.com>
2026-03-01 11:53:19 +0500

    errors resolved

--- src/componenets/Render.jsx
+++ src/componenets/Render.jsx
@@ -2,50 +2,46 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {toast} from "react-toastify";
 import RenderProducts from "./RenderProducts";
-const Render = ({status}) => {  
+const Render = ({status}) => {
   async function getRequest(url) {
     const request = await axios.get(url);
     return request;
   }
-  try {
-    const [renderData, setData] = useState([]);
-    const api = "https://fakestoreapi.com/products";
+  const [renderData, setData] = useState([]);
+  const api = "https://fakestoreapi.com/products";
 
-    useEffect(() => {
-      function getResponse(url) {
-        try {
-          const responseData = getRequest(url)
-            .then((response) => {
-              localStorage.setItem("products", JSON.stringify(response?.data));
-              setData(response?.data);
-            })
-            .catch((error) =>
-              toast.error("Something went wrong!", {
-                autoClose: 2000,
-              }),
-            );
-        } catch (error) {
-          throw new Error("Something went wrong!");
-        }
+  useEffect(() => {
+    function getResponse(url) {
+      try {
+        const responseData = getRequest(url)
+          .then((response) => {
+            localStorage.setItem("products", JSON.stringify(response?.data));
+            setData(response?.data);
+          })
+          .catch((error) =>
+            toast.error("Something went wrong!", {
+              autoClose: 2000,
+            }),
+          );
+      } catch (error) {
+        throw new Error("Something went wrong!");
       }
+    }
 
-      const getProducts = localStorage.getItem("products");
+    const getProducts = localStorage.getItem("products");
 
-      if (getProducts) {
-        setData(JSON.parse(getProducts));
-      } else {
-        getResponse(api);
-      }
-    }, [status]);
+    if (getProducts) {
+      setData(JSON.parse(getProducts));
+    } else {
+      getResponse(api);
+    }
+  }, [status]);
 
-    return (
-      <>
-        <RenderProducts data={renderData} status={status} />
-      </>
-    );
-  } catch (error) {
-    throw new Error(error);
-  }
+  return (
+    <>
+      <RenderProducts data={renderData} status={status} />
+    </>
+  );
 };
 
 export default Render;
